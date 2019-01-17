37d08bf7cfbf8a9f0bdd3783671b9094471354ad
Yi Luo <46770297+Tttttbili@users.noreply.github.com>
2019-01-17 08:45:25 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,16 @@
+/**
+ * Author: Yi Luo
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
   printf("Yi Luo\n
+          computer Science\n");
+
+  return 0;
+}
